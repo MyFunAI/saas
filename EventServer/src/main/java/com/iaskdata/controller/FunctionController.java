d51f0adcbfdf8e2a515ca50d6a4955e734423716
jiangzhenxing <jiangzhenxing@reyun.com>
2016-05-12 18:45:01 +0800

    implement predict

--- EventServer/src/main/java/com/iaskdata/controller/FunctionController.java
+++ EventServer/src/main/java/com/iaskdata/controller/FunctionController.java
@@ -1,5 +1,10 @@
 package com.iaskdata.controller;
 
+import java.util.List;
+
+import hex.genmodel.easy.exception.PredictException;
+
+import com.iaskdata.service.PredictService;
 import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
 
@@ -34,9 +39,20 @@ public class FunctionController extends Controller {
 	 */
 	@ActionKey("/predict")
 	public void predict() {
-		setAttr("status", -1);
-		setAttr("msg", "Unimplemented");
-		setAttr("ts", System.currentTimeMillis());
+		setAttr("status", 0);
+
+		PredictService ps = new PredictService();
+		try {
+			List result = ps
+					.predictHandle("loan_amnt=10000&emp_length=5&home_ownership=RENT&annual_inc=60000&verification_status=verified&purpose=debt_consolidation&addr_state=FL&dti=3&delinq_2yrs=0&revol_util=35&total_acc=4&longest_credit_length=10");
+			setAttr("result", result);
+		} catch (PredictException e) {
+			setAttr("status", -1);
+			setAttr("msg", "Unimplemented");
+			setAttr("ts", System.currentTimeMillis());
+			e.printStackTrace();
+		}
+
 	}
 
 }
